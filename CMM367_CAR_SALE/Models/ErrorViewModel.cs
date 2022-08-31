ead46a6b829fa69a9ebcf09fda5311a2f36b7b5c
piiped <aomkadang@gmail.com>
2022-08-31 20:37:59 +0700

    Add project files.

---
+++ CMM367_CAR_SALE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CMM367_CAR_SALE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
